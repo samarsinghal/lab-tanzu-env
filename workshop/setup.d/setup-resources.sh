6b320b2fa3f4b3400ea0b0795e06297e1f36b3d0
samar singhal <samarsinghal@ssamar-a01.vmware.com>
2021-09-12 22:55:56 -0600

    POC tanzu advance

--- workshop/setup.d/setup-resources.sh
+++ workshop/setup.d/setup-resources.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# envsubst < demo/my-gateway.yaml.in > demo/my-gateway.yaml
-# envsubst < demo/my-gateway-ingress.yaml.in > demo/my-gateway-ingress.yaml
-# envsubst < demo/helloworld-gateway-mapping.yaml.in > demo/helloworld-gateway-mapping.yaml
-# envsubst < demo/github-gateway-mapping.yaml.in > demo/github-gateway-mapping.yaml
-# envsubst < demo/github-route-config.yaml.in > demo/github-route-config.yaml
-# envsubst < demo/github-ratelimit-route-config.yaml.in > demo/github-ratelimit-route-config.yaml
-# envsubst < animal-rescue/base/gateway-demo-ingress.yaml.in > animal-rescue/base/gateway-demo-ingress.yaml
-# envsubst < animal-rescue/base/gateway-demo.yaml.in > animal-rescue/base/gateway-demo.yaml
+envsubst < scenarios/operator/sc01-tc01.md > workshop/content/labs/sc01-tc01.md
+
 
